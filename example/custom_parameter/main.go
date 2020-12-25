ce543f33373658cd5eb65326a72458b066a86a35
wakusei-meron- <b0941015+gihub@gmail.com>
2020-12-25 12:17:10 +0900

    add custom parameter sample

---
+++ example/custom_parameter/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/homie-dev/gotwiml/twiml"
+	"github.com/homie-dev/gotwiml/twiml/attr"
+)
+
+func main() {
+	client := twiml.NewClient("hoge").
+		Parameter(attr.Name("foo"), attr.Value("bar"))
+	resp := twiml.NewVoiceResponse().
+		AppendDial(twiml.NewDial().AppendClient(client))
+	twiml, _ := resp.ToXMLPretty("  ")
+	fmt.Println(twiml)
+	// <?xml version="1.0" encoding="UTF-8"?>
+	// <Response>
+	//   <Dial>
+	//     <Client>hoge
+ 	//       <Parameter name="foo" value="bar"></Parameter>
+	//     </Client>
+	//   </Dial>
+	// </Response>
+}
